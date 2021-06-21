341a110b91c1591e1d756634895419780c275c75
Chee Tey <37930535+cheetiantey@users.noreply.github.com>
2021-06-21 14:14:14 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,9 @@ const About = () => {
     return (
         <div>
             <h4>Version 1.0.0</h4>
+            <a href="https://github.com/cheetiantey/task-tracker" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <br></br>
+            <br></br>
             <Link to="/">Go Back</Link>
         </div>
     )
